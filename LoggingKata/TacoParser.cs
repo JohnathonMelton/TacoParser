6a9c74a8f8206196c0908a586d06ea77cfdea954
Johnathon Melton <seventh.acoustic@gmail.com>
2023-11-29 21:25:18 -0600

    Work in Progress-Stage 1

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -18,35 +18,46 @@
             if (cells.Length < 3)
             {
                 // Log error message and return null
+                logger.LogWarning("Incomplete Data. Less than three elements present.");
                 return null; 
             }
 
             // TODO: Grab the latitude from your array at index 0
+            var latitude = double.Parse(cells[0]);
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
             
             
             // TODO: Grab the longitude from your array at index 1
+            var longitude = double.Parse(cells[1]);
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
             
             
             // TODO: Grab the name from your array at index 2
-            
+            var name = cells[2];
 
-            // TODO: Create a TacoBell class
+
+            // TODO: Create a TacoBell class - Done
             // that conforms to ITrackable
-            
+
             // TODO: Create an instance of the Point Struct
-            // TODO: Set the values of the point correctly (Latitude and Longitude) 
+            // TODO: Set the values of the point correctly (Latitude and Longitude)
+            var point = new Point();
+            point.Latitude = latitude;
+            point.Longitude = longitude;
 
             // TODO: Create an instance of the TacoBell class
             // TODO: Set the values of the class correctly (Name and Location)
+            var tacoBell = new TacoBell();
+            tacoBell.Name = name;
+            tacoBell.Location = point;
+
 
             // TODO: Then, return the instance of your TacoBell class,
             // since it conforms to ITrackable
 
-            return null;
+            return tacoBell;
         }
     }
 }
